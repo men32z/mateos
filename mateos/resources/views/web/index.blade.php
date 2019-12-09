b6f2bf66c574838a10ef51655afcc167da4e94af
Luis Preza <preza.luis@gmail.com>
2019-12-09 13:32:07 -0600

    template index

--- mateos/resources/views/web/index.blade.php
+++ mateos/resources/views/web/index.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-12 mb-2">
+        <img src="/images/lonas/mostrador_frente.jpg" class="home-images" alt="">
+      </div>
+      <div class="col-sm-6">
+        <img src="/images/lonas/lateral_mostrador2.jpg" class="home-images" alt="">
+      </div>
+      <div class="col-sm-6">
+        <img src="/images/lonas/lateral_mostrador.jpg" class="home-images" alt="">
+      </div>
+    </div>
+  </div>
 @endsection
