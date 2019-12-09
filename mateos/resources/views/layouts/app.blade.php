9ec6e5081d2a180576793b9684696228928ed7d0
Luis Preza <preza.luis@gmail.com>
2019-12-09 13:41:21 -0600

    footer simple

--- mateos/resources/views/layouts/app.blade.php
+++ mateos/resources/views/layouts/app.blade.php
@@ -37,6 +37,7 @@
         <main class="py-4">
             @yield('content')
         </main>
+        @include('layouts.footer')
     </div>
 </body>
 </html>
---
+++ mateos/resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<nav class="container-fluid bg-dark color-white p-4 footer-top">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <a href="#">Terminos y condiciones</a>
+        <a href="#">Contacto</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<footer class="footer-bot">
+  
+</footer>
