c1590b9f0e549de33f677722feea2eed023fec68
Luis Preza <preza.luis@gmail.com>
2019-12-09 10:45:14 -0600

    auth, construction view

---
+++ mateos/resources/views/construction.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts/app')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <img src="/images/mad-designer.png" class="img-fluid" alt="">
+      </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 text-center">
+          <h1>Sitio en construcción</h1>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12 col-md-8 offset-md-2 offset-lg-2 col-lg-8">
+          <p class="text-center">gracias por su paciencia, este sitio estará terminado pronto.</p>
+        </div>
+      </div>
+  </div>
+@endsection
