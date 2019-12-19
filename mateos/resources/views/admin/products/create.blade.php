41d82eec5c420c95aa7baba9f9ab9fd446939c44
Luis Preza <preza.luis@gmail.com>
2019-12-18 18:51:54 -0600

    Create new product view

---
+++ mateos/resources/views/admin/products/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app', ['settings' => [
+  'menu_admin' => true
+  ]])
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-12 col-md-6 offset-md-3 bg-white p-2">
+      <h2>Nuevo Producto</h2>
+    </div>
+    <div class="col-12 col-md-6 offset-md-3 bg-white p-2">
+      <form action="{{route('products.store')}}" method="post">
+        @csrf
+        <div class="form-group">
+          <label for="product_name">Nombre</label>
+          <input type="text" id="product_name" name="name" class="form-control" value="">
+        </div>
+        <div class="form-group">
+          <label for="product_description">Descripcion</label>
+          <textarea id="product_description" name="description" class="form-control">
+          </textarea>
+        </div>
+        <input type="submit" value="Guardar" class="btn btn-success mt-2">
+      </form>
+    </div>
+  </div>
+</div>
+@endsection
--- mateos/resources/views/admin/products/index.blade.php
+++ mateos/resources/views/admin/products/index.blade.php
@@ -6,9 +6,13 @@
 <div class="container">
   <div class="row">
     <div class="col-12 col-md-6 offset-md-3 bg-white p-2">
+      <h2>Productos</h2>
+      <br> <a href="{{route('products.create')}}" class="btn btn-success">Nuevo</a>
+    </div>
+    <div class="col-12 col-md-6 offset-md-3 bg-white p-2">
       <table class="table table-striped">
         <thead>
-          <th>Nombre</th>
+          <th scope="col">Nombre</th>
         </thead>
         <tbody>
           @foreach ($products as $product)
