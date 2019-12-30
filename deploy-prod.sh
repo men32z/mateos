59e1a8a88893371fdbaa26f4c7795a2d880137cc
Luis Preza <preza.luis@gmail.com>
2019-12-30 14:17:01 -0600

    production deploy storage link

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,4 +1,10 @@
+# copying files to public_html
 rm -R ../public_html/*
 cp -R mateos/public/. ../public_html
+
+# link to storage
+ln -s /home/mateosenlinea/mateos/mateos/storage/app/public/ /home/mateosenlinea/public_html/storage
+
+# fix index file
 sed -i 's=/../vendor/autoload.php=/../mateos/mateos/vendor/autoload.php=g' ../public_html/index.php
 sed -i 's=/../bootstrap/app.php=/../mateos/mateos/bootstrap/app.php=g' ../public_html/index.php
