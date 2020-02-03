4e7a624f5c01b5612a52dd7d78406e39469e3fff
Luis Preza <preza.luis@gmail.com>
2020-02-03 14:11:50 -0600

    link deploy

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -3,8 +3,8 @@ rm -R ../public_html/*
 cp -R mateos/public/. ../public_html
 
 # link to storage
-#ln -s /home/mateosenlinea/mateos/mateos/storage/app/public/ /home/mateosenlinea/mateos/mateos/public/storage
-ln -s /home/mateosenlinea/mateos/mateos/storage/app/public/ /home/mateosenlinea/public_html/storage
+#ln -s /home/mateosenlinea/mateos/mateos/public/storage /home/mateosenlinea/mateos/mateos/storage/app/public/ 
+ln -s /home/mateosenlinea/public_html/storage /home/mateosenlinea/mateos/mateos/storage/app/public/
 
 # fix index file
 sed -i 's=/../vendor/autoload.php=/../mateos/mateos/vendor/autoload.php=g' ../public_html/index.php
