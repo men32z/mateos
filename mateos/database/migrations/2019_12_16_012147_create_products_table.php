8de7d2999ffcec340a5a4c79b84beb1ec1430973
Luis Preza <preza.luis@gmail.com>
2020-02-18 10:33:05 -0600

    nullable name

--- mateos/database/migrations/2019_12_16_012147_create_products_table.php
+++ mateos/database/migrations/2019_12_16_012147_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name',250);
+            $table->string('name',250)->nullable();
             $table->text('description')->nullable();
             $table->timestamps();
         });
